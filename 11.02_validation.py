cf2d9cc974454a578b426d5c97c4b45b8067919d
DogBrother <roys99141@gmail.com>
2025-11-16 16:35:10 +0800

    update model

--- 11.02_validation.py
+++ 11.02_validation.py
@@ -2,12 +2,12 @@ from ultralytics import YOLO
 
 
 def vaildation():
-    model = YOLO(r"Mango_training\mango_classification_2025-11-13-11.13.12\weights\best.pt")
+    model = YOLO(r"model\mango_detect.pt")
     metric = model.val(
-        data=r"D:\大學\專題\影像辨識芒果\app\mango_dataset\train",
+        # data=r"D:\大學\專題\影像辨識芒果\app\mango_dataset\train",
         project="Mango_valdation",
-        name="classfication_for_inside",
-        imgsz=224,
+        name="object_detection",
+        imgsz=640,
         device=0,
         verbose=True
     )
