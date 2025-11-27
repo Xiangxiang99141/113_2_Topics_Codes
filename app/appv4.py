612b222dd65dc56a0830f5fc221ca7df9590f629
DogBrother <roys99141@gmail.com>
2025-11-27 21:57:54 +0800

    增加 config 修改模型路徑

--- app/appv4.py
+++ app/appv4.py
@@ -2,9 +2,10 @@
 from PySide6.QtWidgets import QApplication
 from PySide6.QtCore import Qt
 import sys
-from pathlib import Path 
+from pathlib import Path
 from view import App
-
+import json
+import os
 cwd_path = Path().cwd()
 if __name__ == '__main__':
     # 設定高 DPI 支援（可選，讓畫面更清晰）
@@ -13,8 +14,13 @@ if __name__ == '__main__':
     )
     
     # model_path = cwd_path / 'model/model.pt'
-    model_path = 'model/model.pt'
-    app = App(sys.argv, model_path)
+    # model_path = 'model/model.pt'
+    
+    with open(f'{os.getcwd()}/config.json') as c:
+        config = json.load(c)['model']
+    model_path = config['path']
+    
+    app = App(sys.argv, f"{os.getcwd()}/{model_path}")
     try:
         sys.exit(app.exec())
     finally:
